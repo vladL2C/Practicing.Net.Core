4cd0274eeb3c795b28a393a8a467c4d489b570f8
Vlad Saraev <vlad.saraev@xero.com>
2020-08-11 17:15:02 +1200

    s

--- Practice.API/Models/Users.cs
+++ Practice.API/Models/Users.cs
@@ -2,25 +2,22 @@
 using MongoDB.Bson.Serialization.Attributes;
 using Practice.API.Attributes;
 
+
 namespace Practice.API.Models
 {
-    [BsonCollection("animals")]
-    public class Animal
+    [BsonCollection("users")]
+    public class Users
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         [BsonElement]
         public string Id { get; set; }
 
-        public string Name { get; set; }
-
-        public string Type { get; set; }
+        public string UserName { get; set; }
 
         public int Phone { get; set; }
 
         public string Email { get; set; }
-
-        public string UserName { get; set; }
 
     }
 }
